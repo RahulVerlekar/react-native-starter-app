d6075f95185e7e93005a698a1f81c6ff7499aaf1
rahul <rahulverlekar@gmail.com>
2025-03-09 14:50:20 +0530

    Added Home and login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Index' }} />
+      <Stack.Screen name="login" options={{ title: 'Login' }} />
+      <Stack.Screen name="home" options={{ title: 'Home' }} />
+    </Stack>
+  );
 }
